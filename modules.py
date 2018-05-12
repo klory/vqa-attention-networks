b49e766d0e88748c8215df4483750e55b8f1c09f
cobyC <shenchang724@hotmail.com>
2018-05-12 13:08:43 -0400

    attention model 2

--- modules.py
+++ modules.py
@@ -3,21 +3,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import pdb
+import sys
+
 class Attention_layer(nn.Module):
-    def __init__(self, att_type=1):
+    def __init__(self, feature_size, att_type=1):
         super(Attention_layer, self).__init__()
         if att_type == 1:
-            self.att_layer = Attention_1()
+            self.att_layer = Attention_1(feature_size)
         elif att_type == 2:
-            self.att_layer = Attention_2()
+            self.att_layer = Attention_2(feature_size)
         else:
-            print("Attention type %d not supported." % att_type)
-            break
+            sys.exit(0)
+            
+        self.nonlinear_1 = Nonlinear_layer(feature_size)
+        self.nonlinear_2 = Nonlinear_layer(feature_size)
 
-        self.fc1 = Linear_layer()
-        self.fc2 = Linear_layer()
-        self.nonlinear_1 = Nonlinear_layer()
-        self.nonlinear_2 = Nonlinear_layer()
+    def forward(self, feature_1, feature_2):
+        feature_1_embbed = self.nonlinear_1(feature_1)
+        feature_2_embbed = self.nonlinear_2(feature_2)
+
+        f_hat, att = self.att_layer(feature_1, feature_2)
+
+        return (feature_1_embbed + f_hat, feature_2_embbed, att)
+
+class Attention_1(nn.Module):
+    def __init__(self, feature_size):
+        super(Attention_1, self).__init__()
+        self.fc = nn.Linear(feature_size, 1)
 
     def forward(self, feature_1, feature_2):
         """
@@ -25,66 +37,52 @@ class Attention_layer(nn.Module):
             feature_1: N, T, V
             featrue_2: N, L, D
         """
-        feature_1_embbed = self.nonlinear_1((self.fc1(feature_1, V, V), V, V)
-        feature_2_embbed = self.nonlinear_2(self.fc2(feature_2, D, D), D, D)
+        L, D = feature_1.shape[1], feature_1.shape[2]
+        T, V = feature_2.shape[1], feature_2.shape[2]
+        assert (D == V), "dimension of feature_1 and feature_2 not match"
 
-        return (feature_1_embbed + self.att_layer(feature_1, feature_2), feature_2_embbed)
+        feature2 = torch.unsqueeze(feature_2, 2) # N, T, 1, V,
+        feature2 = feature2.repeat(1, 1, L, 1) # N, T, L, V
 
-class Attention_1(nn.Module):
-    def __init__(self):
-        super(Attention_1, self).__init__()
-        self.fc = Linear_layer()
-
-    def forward(self, feature_1, feature_2):
-        T, V = feature_1.shape[1], qes_features.shape[2]
-        L, D = feature_2.shape[1], img_features.shape[2]
-        assert (D == V), "dimension of img_features and q_features not match"
-
-        # conver to pytorch tensor
-        feature_1 = torch.tensor(feature_1, dtype=torch.float)
-        feature_2 = torch.tensor(feature_2, dtype=torch.float)
-
-        feature1 = torch.unsqueeze(feature_1, 2) # N, T, 1, V,
-        feature1 = features1.repeat(1, 1, L, 1) # N, T, L, V
-
-        feature2 = torch.unsqueeze(feature_2, 0).view(-1, 1, L, D) # N, 1, L, D
-        feature2 = feature2.repeat(1, T, 1, 1) # N, T, L, D
+        feature1 = torch.unsqueeze(feature_1, 0).permute(1, 0, 2, 3) # N, 1, L, D
+        feature1 = feature1.repeat(1, T, 1, 1) # N, T, L, D
 
         h_temp = feature1 + feature2
 
         # compute attention
-        att = self.fc(h_temp, D, 1) # N, T, L
+        att = torch.squeeze(self.fc(h_temp)) # N, T, L
         att = F.softmax(att, dim=2)
 
-        I_hat = torch.matmul(att, features_2) # N, T, D
-        return I_hat
+        f_hat = torch.matmul(att, feature_1) # N, T, D
+        return f_hat, att
 
 class Attention_2(nn.Module):
-    def __init__(self):
+    def __init__(self, feature_size):
         super(Attention_2, self).__init__()
-        pass
+        self.fc1 = nn.Linear(feature_size, feature_size, bias=False)
+        self.fc2 = nn.Linear(feature_size, 1)
 
-    def forward(self):
-        pass
+    def forward(self, feature_1, feature_2):
+        L, D = feature_1.shape[1], feature_1.shape[2]
+        T, V = feature_2.shape[1], feature_2.shape[2]
+        assert (D == V), "dimension of img_feature and q_feature not match"
+
+        feature1 = self.fc1(feature_1) # N, L, D
+        att = torch.matmul(feature_2, feature1.permute(0, 2, 1)) # N, T, L
+        att = F.softmax(att, dim=2) # N, T, L
+
+        f_hat = torch.matmul(att, feature_1) # N, T, D
+        return f_hat, att
+
 class Nonlinear_layer(nn.Module):
-    def __init__(self):
+    def __init__(self, f_size):
         super(Nonlinear_layer, self).__init__()
-        self.fc1 = Linear_layer()
-        self.fc2 = Linear_layer()
+        self.fc1 = nn.Linear(f_size, f_size)
+        self.fc2 = nn.Linear(f_size, f_size)
 
-    def forward(self, inputs, in_dim, out_dim):
-        o_1 = self.fc1(inputs, in_dim, out_dim)
-        o_2 = self.fc2(inputs, in_dim, out_dim)
+    def forward(self, inputs):
+        o_1 = self.fc1(inputs)
+        o_2 = self.fc2(inputs)
         o_tanh = torch.tanh(o_1)
         o_sig = torch.sigmoid(o_2)
-        return o = o_tanh * o_sig
-
-class Linear_layer(nn.Module):
-    def __init__(self):
-        super(Linear_layer, self).__init__()
-        
-    def forward(self, inputs, in_dim, out_dim):
-        weights = torch.empty(out_dim, in_dim)
-        nn.init.xavier_uniform_(weights)
-        o = torch.squeeze(F.linear(inputs, weights))
-        return o
+        return o_tanh * o_sig
