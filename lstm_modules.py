7da1b1c991921be24e1b4c5c4f369c787d9cb497
cobyC <shenchang724@hotmail.com>
2018-05-21 17:21:39 -0400

    vis lstm model

--- lstm_modules.py
+++ lstm_modules.py
@@ -3,39 +3,58 @@ import torch.nn as nn
 from torch import optim
 
 import pdb
-class LSTM(nn.Module):
-    def __init__(self, hidden_dim=512, embed_dim=512, vocab_size=6000, batch_size=16):
-        super(LSTM, self).__init__()
+class VisLSTM(nn.Module):
+    def __init__(self, img_dim=4096, hidden_dim=512, embed_dim=512, vocab_size=6000):
+        super(VisLSTM, self).__init__()
         # parameters of lstm
-        self.hidden_dim = hidden_dim
-        self.embed_dim = embed_dim
-        self.vocab_size = vocab_size
-        self.batch_size = batch_size
+        self.hidden_dim = hidden_dim # H
+        self.embed_dim = embed_dim # V
+        self.vocab_size = vocab_size  # E
+        self.img_dim = img_dim # I
 
         # network graph
-        self.embedding = nn.Embedding(self.vocab_size, self.embed_dim)
+        self.embedding_ques = nn.Embedding(self.vocab_size, self.embed_dim)
+        self.embedding_img = nn.Linear(self.img_dim, self.hidden_dim)
         self.lstm = nn.LSTMCell(self.embed_dim, self.hidden_dim)
         self.output_layer = nn.Linear(self.hidden_dim, self.vocab_size)
 
-    def init_hidden(self):
-        return (torch.zeros(self.batch_size, self.hidden_dim),\
-                torch.zeros(self.batch_size, self.hidden_dim))
+    def init_hidden(self, batch_size):
+        return (torch.zeros(batch_size, self.hidden_dim),\
+                torch.zeros(batch_size, self.hidden_dim))
 
-    def forward(self, inputs):
+    def forward(self, questions, img_features, first_words=True):
         """
-        inputs: N, T, V
+        inputs: 
+            questions: N, T
+            img_features: N, D
+            first_words: whether img_features fed into lstm as the first words or not
         """
-        max_len = inputs.shape[1]
-        embedded_inputs = self.embedding(inputs).view(max_len, self.batch_size, -1) # T, N, E
-        h = self.init_hidden()
+        embedded_ques = self.embedding_ques(questions) # N, T, V
+        embedded_img = self.embedding_img(img_features) # N, H
+        embedded_ques = embedded_ques.permute((1, 0, 2))
+
+        N, D = embedded_img.shape
+        T, N, V = embedded_ques.shape
+        assert(D == V), 'question embedding dimension and image feature dimension not match'
+
+        inputs = torch.zeros((T+1, N, V ))
+        #pdb.set_trace()
+        if first_words:
+            inputs[0] = embedded_img
+            inputs[1:, :, :] = embedded_ques
+        else:
+            inputs[:T, :, :] = embedded_ques
+            inputs[T] = embedded_img
+
+        h = self.init_hidden(N)
         hidden_states = list()
-        for t in range(max_len):
-            h = self.lstm(embedded_inputs[t:t+1], h)
+        for t in range(T+1):
+            h = self.lstm(inputs[t], h)
             hidden_states.append(h[0])
 
-        # hi
-        hidden_states = torch.cat(hidden_states, 0).view(self.batch_size, max_len, -1)
-        return hidden_states
+        hidden_states = torch.stack(hidden_states, 1)
+        output = self.output_layer(hidden_states[:, T, :])
+        return output, hidden_states
 
 class LSTM_Attention(nn.Module):
     def __init__(self, hidden_dim=512, embed_dim=512, vocab_size=6000, batch_size=16, dropout_rate=0.5):
